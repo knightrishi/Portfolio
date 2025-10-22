784ac66e87d8ecf25a65a68c75f0d1e84345fc8b
knightrishi <arnav.ara05@gmail.com>
2025-10-22 12:44:16 +0530

    Added Skills

---
+++ Portfolio/src/sections/Skills.jsx
@@ -0,0 +1,20 @@
+
+function Skills() {
+  return(
+    <section id='skillss' className='py-20 border-t border-gray-800'>
+      <h3 className='text-3xl font-semibold font-pixel text-[#00CED1] mb-4'>My Skills </h3>
+    
+        <ul  className="max-w-2xl mx-auto space-y-3 text-cream">
+            <li>Languages:C,C++,Java,GOLang(learning),JavaScript</li>
+            <li>Frontend: HTML, CSS, Tailwind CSS, React</li>
+            <li>Backend & Frameworks: JSP, JDBC,Node.js</li>
+            <li>Java Development: Core Java, JavaFX</li>
+            <li>Tools & Platforms: Git, GitHub, Docker</li>
+            <li>Libraries & APIs: Tesseract (OCR), ZXing (QR/Barcode Processing)</li>
+        </ul>
+     
+        </section>
+        );
+}
+
+export default Skills;
