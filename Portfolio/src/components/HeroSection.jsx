7ff4c20a307d6c9786780ca1f894cc7c7259524e
knightrishi <arnav.ara05@gmail.com>
2025-10-22 22:54:37 +0530

    Final Modification

--- Portfolio/src/components/HeroSection.jsx
+++ Portfolio/src/components/HeroSection.jsx
@@ -1,24 +1,40 @@
 import { motion } from "framer-motion";
 import React, { useState } from "react";
+import { Typewriter } from "react-simple-typewriter";
 import herorun from "../utils/herorun.gif"
 import herostill1 from "../utils/herostill1.png"
+
+
+
+
 const HeroSection = () => {
   const [isRunning ,setIsRunning]=useState(true);
   const handleClick =()=>{
 setIsRunning(true);
-setTimeout(()=> setIsRunning(false),10000);
+setTimeout(()=> setIsRunning(false),100000);
   }
   return (
     <section
-      id="hero"
-      className="min-h-[90vh] flex flex-col justify-center items-center text-center space-y-6"
+      id="hero" 
+      className="min-h-[60vh] flex flex-col justify-center items-center text-center space-y-6 bg-[#0A2540]"
     >
       <div className="animate-hero-entry" onClick={handleClick}>
         <img
           src={isRunning ? herorun : herostill1}
           alt="Retro Hero"
-          className="w-48 h-auto drop-shadow-[0_0_15px_#00CED1]"
+          className="w-28 h-auto drop-shadow-[0_0_15px_#00CED1]"
         />
+         <h1 className="text-3xl sm:text-4xl font-bold font-pixel text-[cream] mt-4">
+          <Typewriter
+            words={["Hi... I am Arnav Singh"]}
+            loop={100}          // type once
+            cursor            // show blinking cursor
+            cursorStyle="|"   // style of cursor
+            typeSpeed={200}   // speed of typing (ms per letter)
+            deleteSpeed={70}  // speed of deleting (not needed here)
+          />
+        </h1>
+        
       </div>
 
       <motion.p
@@ -27,20 +43,13 @@ setTimeout(()=> setIsRunning(false),10000);
         transition={{ delay: 0.6, duration: 1 }}
         className="text-[#FFFFFF] text-sm sm:text-base max-w-xl"
       >
-        A retro-futuristic developer crafting digital dreams with pixels and purpose.
+        <p>I am a passionate software developer who believes code can have emotion — a bridge between
+          machines and meaning.I chase elegance in logic, and beauty in simplicity.
+          I believe i have a long way ahead but i am ready to handel the voilent waves of the code and triumph this coding voyage.</p>
+       
       </motion.p>
 
-      <motion.a
-        href="#projects"
-        whileHover={{
-          scale: 1.2,
-          textShadow: "0 0 10px #00ffff",
-        }}
-                transition={{ type: "tween", duration: 0.5, ease: "easeInOut" }}
-        className="mt-6 border border-[#00CED1] px-6 py-3 rounded-lg text-[#FFFFFF] hover:bg-[#00CED1] hover:text-[#141212] font-bold duration-300"
-      >
-        View Projects
-      </motion.a>
+      
     </section>
   );
 };
